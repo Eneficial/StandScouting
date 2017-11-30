68f20cb203b057930265d51d97ccc8c037a0da20
MoSadie <sean@cushmantech.net>
2017-11-29 20:33:29 -0800

    Most of the major changes done, untested. Small problem, Need to add cmp events to event list as well.

--- Assets/DataStorage.cs
+++ Assets/DataStorage.cs
@@ -9,16 +9,15 @@ public class DataStorage : MonoBehaviour {
 	public Dictionary<string,string> data = new Dictionary<string, string>();
 	public Dictionary<string,DataInput> inputs = new Dictionary<string, DataInput> ();
 	public string serverBaseURL = "";
-    public string Version = "v1.1";
+    public string[] Version = { "2017", "1", "0" };
     public ResultText uploadResultText;
 	// Use this for initialization
 	void Start () {
 		//data = new Dictionary<string, string>();
 
 		//Setup required default values, in case weird things happen
-		data.Add("Version", Version);
-		data.Add("ScouterName", "");
-		data.Add("TeamNumber", "0");
+		data.Add("Version", JsonUtility.ToJson(Version));
+        data.Add("ScouterName", "");
 	}
 
 	public bool addData(string key, string value, bool overwrite) {
@@ -84,13 +83,18 @@ public class DataStorage : MonoBehaviour {
 		}
 		using (StreamWriter sw = File.CreateText (filePath)) {
 			foreach (KeyValuePair<string,string> kvp in data) {
-				if (inputs.ContainsKey (kvp.Key) && clear && kvp.Key != "ScouterName" && kvp.Key != "Version")
+				if (inputs.ContainsKey (kvp.Key) && clear && !isStaticKey(kvp.Key))
 					inputs [kvp.Key].clearData();
 				sw.WriteLine (kvp.Key + ";" + kvp.Value.Replace(';',':'));
 			}
 		}
 		return filePath;
 	}
+
+    private bool isStaticKey(string key)
+    {
+        return key == "ScouterName" || key == "Version" || key == "EventKey";
+    }
 
 	public void sync() {
         StartCoroutine(downloadJSON());
@@ -100,14 +104,28 @@ public class DataStorage : MonoBehaviour {
     /**
      * Downloads data from the scouting server, for example version info and event and team lists.
      **/
-    IEnumerable downloadJSON()
+    public IEnumerator downloadJSON()
     {
+        Debug.Log("Starting download of JSON");
         UnityWebRequest wwwRequest = UnityWebRequest.Get(serverBaseURL + "/api/v1/syncDownload.php");
         yield return wwwRequest.SendWebRequest();
 
-        if (wwwRequest.error != null)
+        Debug.Log("Download Completed.");
+        if (!wwwRequest.isHttpError)
         {
-            object[] data = JsonUtility.FromJson<object[]>(wwwRequest.downloadHandler.text);
+            SyncData data = JsonUtility.FromJson<SyncData>(wwwRequest.downloadHandler.text);
+            GetComponent<EventTeamData>().clearData();
+            GetComponent<EventTeamData>().loadData(data);
+
+            if (File.Exists(Application.persistentDataPath + Path.DirectorySeparatorChar + "data.json"))
+                File.Delete(Application.persistentDataPath + Path.DirectorySeparatorChar + "data.json");
+
+            StreamWriter sw = File.CreateText(Application.persistentDataPath + Path.DirectorySeparatorChar + "data.json");
+            sw.Write(wwwRequest.downloadHandler.text);
+            sw.Close();
+        } else
+        {
+            Debug.LogError(wwwRequest.error);
         }
 
     }
@@ -116,7 +134,7 @@ public class DataStorage : MonoBehaviour {
          * Uploads the data to the remote server set in the dataPOSTurl variable
          * Returns true if the upload is successful, false if not.
          **/
-        IEnumerator uploadData() {
+        public IEnumerator uploadData() {
 		DirectoryInfo dinfo = new DirectoryInfo(Application.persistentDataPath);
         
 		foreach (FileInfo file in dinfo.GetFiles()) {
@@ -145,7 +163,7 @@ public class DataStorage : MonoBehaviour {
 			}
 
 
-            WWW wwwRequest = new WWW(dataPOSTurl, form);
+            WWW wwwRequest = new WWW(serverBaseURL+"/submit.php", form);
             yield return wwwRequest;
 
             if (wwwRequest.error != null) {
@@ -169,35 +187,9 @@ public class DataStorage : MonoBehaviour {
 }
 
 [System.Serializable]
-class SyncData
+public class SyncData
 {
     public string[] CurrentVersion;
     public EventData[] Events;
-    public EventTeamList[] TeamsByEvent
-}
-
-[System.Serializable]
-class EventData {
-    public string city;
-    public string country;
-    public DistrictInfo district;
-    public string end_date;
-    public string event_code;
-    public string event_type;
-    public string key;
-    public string name;
-    public string start_date;
-    public string year;
-}
-
-[System.Serializable]
-class EventTeamList
-{
-
-}
-
-[System.Serializable]
-class DistrictInfo
-{
-
+    public EventTeamList[] TeamsByEvent;
 }
